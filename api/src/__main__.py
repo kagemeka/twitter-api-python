5ab9e9e3da778235d03c4a4d24990a53351cb38c
kagemeka <kagemeka1@gmail.com>
2021-07-07 14:03:09 +0900

    enhance: fetch comic keywords from s3 (#25)

--- api/src/__main__.py
+++ api/src/__main__.py
@@ -1,35 +1,7 @@
-from __future__ import (
-  annotations,
-)
-import typing
-import dataclasses
-from typing import (
-  Optional,
-)
-from datetime import (
-  datetime,
-)
-import boto3
-
-import pandas as pd
-
 from pprint import (
   pprint,
 )
 
-
-
-import enum
-from enum import (
-  auto,
-)
-
-
-
-from dataclasses import (
-  asdict,
-  fields,
-)
 
 
 from lib.adam import (
@@ -37,12 +9,74 @@ from lib.adam import (
 )
 
 
+import typing
+import boto3
+import pandas as pd
+
+
+class FetchComicKeywords():
+  def __call__(
+    self,
+  ) -> typing.List[str]:
+    self.__donwload_csv()
+    self.__read_csv()
+    self.__get_keywords()
+    return self.__keywords
+  
+
+  def __get_keywords(
+    self,
+  ) -> typing.NoReturn:
+    df = self.__df
+    words = df.keyword.values
+    self.__keywords = words
+    
+
+  def __read_csv(
+    self,
+  ) -> typing.NoReturn:
+    self.__df = pd.read_csv(
+      self.__save_path,
+    )
+  
+
+  def __donwload_csv(
+    self,
+  ) -> typing.NoReturn:
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(
+      'av-adam-entrance',
+    )
+    ls = bucket.objects.filter(
+      Prefix='natalie/',
+    )
+    ls = [o.key for o in ls]
+    ls.sort()
+    obj = ls[-1]
+    self.__save_path = (
+      '/tmp/data.csv'
+    )
+    bucket.Object(
+      obj,
+    ).download_file(
+      self.__save_path,
+    )
+
+
+
 
 def main():
-  get = GetUserInfos()
-  users = get()
+  # get = GetUserInfos()
+  # users = get()
   
-  pprint(users)
+  # pprint(users)
+
+  fetch = FetchComicKeywords()
+  words = fetch()
+  pprint(words)
+
+
+
 
   # s3 = boto3.resource('s3')
   # bucket = s3.Bucket(
