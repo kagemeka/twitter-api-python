4b99c5f39d9f38ed9fbeb1cb672713a4bab1cda4
kagemeka <kagemeka1@gmail.com>
2021-10-07 02:30:11 +0900

    enhance: implement DatetimeFromStr

--- api/src/lib/__init__.py
+++ api/src/lib/__init__.py
@@ -1,9 +1,13 @@
 import datetime 
 
 
+class DatetimeFromStr():
+  @staticmethod
+  def rfc3339_format(s: str) -> datetime.datetime:
+    f = '%Y-%m-%dT%H:%M:%S.%fZ'
+    return datetime.strptime(s, f)
+
+
 class DatetimeToRFC3339():
-  def __call__(
-    self,
-    dt: datetime.datetime,
-  ) -> str:
+  def __call__(self, dt: datetime.datetime) -> str:
     return f'{dt.isoformat()}Z'
